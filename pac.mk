76f2cd4d53c6cbbb3b007c14afa372c77d24a573
Shreps <shr3ps@gmail.com>
2017-01-26 12:32:15 +0100

    PAC-6.0 env compile bringup

--- pac.mk
+++ pac.mk
@@ -20,17 +20,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # for specific
 $(call inherit-product, device/moto/griffin/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 # Time Zone data for recovery
 PRODUCT_COPY_FILES += \
     bionic/libc/zoneinfo/tzdata:root/system/usr/share/zoneinfo/tzdata
 
-PRODUCT_NAME := cm_griffin
+PRODUCT_NAME := pac_griffin
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := griffin
-PRODUCT_MODEL := XT1650
+PRODUCT_MODEL := Moto Z Droid
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
@@ -42,3 +43,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=griffin PRODUCT_NAME=griffin
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/griffin/griffin:6.0.1/MPL24.246-20/21:user/release-keys \
     PRIVATE_BUILD_DESC="griffin-user 6.0.1 MPL24.246-20 21 release-keys"
+
